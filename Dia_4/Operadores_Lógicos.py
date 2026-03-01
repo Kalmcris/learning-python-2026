83590c0ba07e56d9016fbef3f5d8980c70acfe7f
crdev <CristianOssesSaavedra1989@gmail.com>
2026-02-28 23:46:10 -0300

    Avance Dia 4

---
+++ Dia_4/Operadores_Lógicos.py
@@ -0,0 +1,10 @@
+mi_bool = 4 < 5 and 5 > 6
+print(mi_bool)
+
+mi_bool2 = 1== 10 or 5 > 3
+print(mi_bool2)
+
+texto = "esta frase es verdadera"
+
+mi_bool3 = ("frase" in texto) and ("falsa" not in texto)
+print(mi_bool3)
