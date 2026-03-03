68eb1821f018c9040e7eb989067230bf41ff3707
crdev <CristianOssesSaavedra1989@gmail.com>
2026-03-02 22:55:58 -0300

    Avances

---
+++ Dia_4/Loop_For.py
@@ -0,0 +1,23 @@
+#lista = ["a", "b", "c"]
+
+#for letra in lista:
+ #   numero_letra = lista.index(letra) + 1
+#    print(f"Letra: {numero_letra }: {letra}")
+
+#lista = ["pablo", "laura", "pedro", "ana"]
+
+#for nombre in lista:
+#    if nombre.startswith("p"):
+#        print(nombre)
+
+#numeros = [1, 2, 3, 4, 5]
+#mi_valor = 0
+#for numero in numeros:
+#    mi_valor = mi_valor + numero
+#print(mi_valor)
+
+
+palabra = "python"
+
+for letra in palabra:
+    print(letra)
---
+++ Dia_4/Tomar_Decisiones.py
@@ -0,0 +1,19 @@
+#mascota = "perro"
+
+#if mascota == "gato":
+#   print("Tienes un gato")
+#elif mascota == "perro":
+#    print("Tienes un perro")
+#else:   
+#    print("No tienes ni un gato ni un perro")
+
+edad = 16
+calificacion = 9
+if edad < 18:
+    print("Eres menor de edad")
+    if calificacion >= 7:
+        print("Aprobado")
+    else:
+        print("No aprobado")
+else:
+    print("Eres mayor de edad")
