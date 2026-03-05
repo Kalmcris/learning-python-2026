7080c0c401bdfbdcd8338e09e48d4b82e5d09241
crdev <CristianOssesSaavedra1989@gmail.com>
2026-03-04 22:10:22 -0300

    Avances hasta zip

--- Dia_4/ZIp.py
+++ Dia_4/ZIp.py
@@ -1,6 +1,13 @@
 nombres = ["Juan", "María", "Pedro"]
 edades = [25, 30, 35]
+ciudades = ["Madrid", "Barcelona", "Valencia"]
 
-combinados = zip(nombres, edades)
 
-print(combinados)
+combinados = list(zip(nombres, edades,ciudades))
+
+for nombre, edad, ciudad in combinados:
+    print(f"{nombre} tiene {edad} años y vive en {ciudad}.")
+
+
+
+
