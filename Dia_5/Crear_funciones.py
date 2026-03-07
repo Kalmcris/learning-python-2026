d615cd5fedc13035a1580f6351f0d966468c9e0e
crdev <CristianOssesSaavedra1989@gmail.com>
2026-03-07 09:31:44 -0300

    Avance día 5

---
+++ Dia_5/Crear_funciones.py
@@ -0,0 +1,9 @@
+#Introducción a funciones
+def saludar_persona(nombre):
+ #Esta función saluda a una persona
+ print (f"Hola, {nombre}")
+
+saludar_persona("Fernando") 
+#GIthub ya no me da nada god
+#Ahora si me da funciones
+#Ctrl + Shift + Alt + C para activar o desactivar sugerencias de código
---
+++ Dia_5/Invertir_palabra.py
@@ -0,0 +1,8 @@
+def invertirpalabra(palabra):
+    invertida = palabra[::-1].upper()
+    print(invertida)
+    return invertida
+
+palabra = "Python"
+invertirpalabra(palabra)
+
