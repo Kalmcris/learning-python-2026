255d2d9e96f51d7665dc349d0def3226d605a334
crdev <CristianOssesSaavedra1989@gmail.com>
2026-03-11 10:04:54 -0300

    Primer commit desde ccp

---
+++ Dia_6/Escribir_Archivos.py
@@ -0,0 +1,15 @@
+#archivo = open('prueba.txt','w')
+
+#lista = ['hola','mundo','aqui','estoy']
+
+#for p in lista:
+#    archivo.writelines (p + '\n')
+
+#archivo.close()
+
+#Sistema de logs
+archivo = open('prueba.txt','a')
+
+archivo.write('bienvenido')
+
+archivo.close()
