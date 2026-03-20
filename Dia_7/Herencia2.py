19de98e8271984fb6949efbd1e73f753100d1a58
crdev <CristianOssesSaavedra1989@gmail.com>
2026-03-20 14:47:30 -0300

    Polimorfismo

---
+++ Dia_7/Herencia2.py
@@ -0,0 +1,36 @@
+class Animal:
+
+    def __init__(self,edad,color):
+        self.edad = edad
+        self.color = color
+
+    def nacer(self):
+        print('Este animal ha nacido')
+
+    def hablar(self):
+        print('Este animal emite un sonido')
+
+class Pajaro(Animal):
+    
+    def __init__(self,edad,color,altura_vuelo):
+       super().__init__(edad,color)
+       self.altura_vuelo = altura_vuelo
+       
+       # self.edad = edad
+        #self.color = color
+       
+    
+    def hablar(self):
+        print('pio')
+
+
+
+    def volar(self,metros):
+        print(f'El pajaro vuela {metros} metros')
+
+piolin = Pajaro(3,'amarillo',70)
+
+mi_animal = Animal(5, 'negro')
+
+piolin.volar(100)
+
